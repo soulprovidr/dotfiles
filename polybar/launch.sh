7d1e99011dfe00059707afbb94ea511131fd064c
Shola Anozie <shola@soulprovidr.dev>
2020-05-27 18:44:46 -0500

    Minimal config.

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -5,7 +5,8 @@ killall polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main &
-polybar secondary &
+polybar bspwm-1 &
+polybar bspwm-2 &
+# polybar xwindow-1 &
 
 echo "Bars launched."
